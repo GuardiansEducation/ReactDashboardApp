97706309257d5582e21f1e4d48c23253ee4bf714
architect-prog <maxim.lushh@gmail.com>
2025-02-07 17:47:00 +0100

    update styling approach

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { MantineProvider } from "@mantine/core";
+import { AppShell, Burger, createTheme, MantineProvider, Paper, Title } from "@mantine/core";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { Layout } from "@components";
@@ -7,10 +7,73 @@ import "@mantine/core/styles.css";
 import "./App.css";
 
 function App() {
+  const primaryBackground = "#232233";
+  const secondaryBackround = "#1b1a27";
+  const theme = createTheme({
+    colors: {
+      tomato: [
+        "#ffeae6",
+        "#ffd4cf",
+        "#fea99e",
+        "#fb7a69",
+        "#f8523c",
+        "#f73920",
+        "#f72a10",
+        "#dd1c05",
+        "#c51402",
+        "#ad0600",
+      ],
+    },
+    defaultGradient: {
+      from: "tomato",
+      to: "orange",
+      deg: 90,
+    },
+    components: {
+      AppShell: AppShell.extend({
+        styles: {
+          header: {
+            background: primaryBackground,
+            borderColor: secondaryBackround,
+            borderWidth: "3px",
+          },
+          navbar: {
+            background: primaryBackground,
+            borderColor: secondaryBackround,
+            borderWidth: "3px",
+          },
+          main: {
+            background: secondaryBackround,
+          },
+        },
+      }),
+      Paper: Paper.extend({
+        styles: {
+          root: {
+            background: primaryBackground,
+            borderColor: primaryBackground,
+          },
+        },
+      }),
+      Title: Title.extend({
+        styles: {
+          root: {
+            color: "tomato",
+          },
+        },
+      }),
+      Burger: Burger.extend({
+        defaultProps: {
+          color: "tomato",
+        },
+      }),
+    },
+  });
+
   return (
     <BrowserRouter>
       <Provider store={store}>
-        <MantineProvider>
+        <MantineProvider theme={theme}>
           <Layout />
         </MantineProvider>
       </Provider>
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { routes } from "@constants";
 import { useDisclosure } from "@mantine/hooks";
-import { AppShell, Burger, Group, Image } from "@mantine/core";
+import { AppShell, Burger, Group, Image, MantineTheme, Title } from "@mantine/core";
 import { DashboardRouter, NavigateButton } from "@components";
-import logo from "/logo.png";
+import logo from "/logo.svg";
 
 const navigationLinks = [
   {
@@ -32,30 +32,15 @@ const Layout: React.FC = () => {
     >
       <AppShell.Header>
         <Group h="100%" px="md">
-          <Burger
-            opened={mobileOpened}
-            onClick={toggleMobile}
-            hiddenFrom="sm"
-            size="sm"
-          />
-          <Burger
-            opened={desktopOpened}
-            onClick={toggleDesktop}
-            visibleFrom="sm"
-            size="sm"
-          />
+          <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
+          <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
           <Image src={logo} alt="logo" h={50} radius="md" />
+          <Title size={"lg"}>Football Dashboard</Title>
         </Group>
       </AppShell.Header>
       <AppShell.Navbar p="md">
         {navigationLinks.map((item, index) => (
-          <NavigateButton
-            variant="gradient"
-            gradient={{ from: "red", to: "orange", deg: 90 }}
-            mt="sm"
-            key={index}
-            navigateTo={item.route}
-          >
+          <NavigateButton variant="gradient" mt="sm" key={index} navigateTo={item.route}>
             {item.text}
           </NavigateButton>
         ))}
