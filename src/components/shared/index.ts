59a5480da8a01b957850dcb1729d839961c40bd7
Artsem Kanapliou <k.werocky@gmail.com>
2025-02-13 17:31:09 +0100

    Add loading state to the FavoriteTeams page

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -2,3 +2,4 @@ export { default as NavigateButton } from "./NavigationButton";
 export { default as AreaSelector } from "./AreaSelector";
 export { default as CommonAlert } from "./CommonAlert";
 export { default as CountryFlag } from "./CountryFlag";
+export { default as OverviewLoader } from "./OverviewLoader";
--- src/pages/overview/FavoriteTeams/FavoriteTeams.tsx
+++ src/pages/overview/FavoriteTeams/FavoriteTeams.tsx
@@ -3,8 +3,8 @@ import { Center, Flex } from "@mantine/core";
 import { useEffect, useState } from "react";
 import { SubscribedTeam, TeamListItem, TeamMatches } from "@types";
 import { TeamService } from "@services";
+import { CommonAlert, OverviewLoader } from "@components";
 import TeamCard from "./components/TeamCard";
-import { CommonAlert } from "@components";
 
 interface TeamData {
   team: TeamListItem;
@@ -13,10 +13,14 @@ interface TeamData {
 
 const FavoriteTeams: React.FC = () => {
   const [teamsData, updateTeamsData] = useState<TeamData[]>([]);
+  const [isLoading, setLoading] = useState<boolean>(false);
+
   const favoriteTeams = useAppSelector((state) => state.subscription.selectedOverviewCompetition?.teams || []);
   const hasFavoriteTeams = favoriteTeams.length > 0;
 
   const loadTeamsData = async (subscribedTeams: SubscribedTeam[]) => {
+    setLoading(true);
+
     const dataRequests = subscribedTeams.map(async subscribedTeam => {
       const team = await TeamService.getTeamInfo(subscribedTeam.id);
       const teamMatches = await TeamService.getTeamMatches(subscribedTeam.id);
@@ -32,29 +36,41 @@ const FavoriteTeams: React.FC = () => {
     const teamsData = await Promise.all(dataRequests);
 
     updateTeamsData(teamsData);
+
+    setLoading(false);
   };
 
   useEffect(() => {
     if (favoriteTeams.length > 0) {
-      console.log("Executed");
       loadTeamsData(favoriteTeams);
     }
   }, [favoriteTeams]);
 
-  return (
-    hasFavoriteTeams ?
-      <Flex mt="md" justify="center" wrap="wrap" gap="sm" direction={{ base: "column", lg: "row" }}>
-        {teamsData.map(({ team, teamMatches }, index) => (
-          <Flex key={index} flex={{ base: "1 1 100%", lg: "1 1 40%" }}>
-            <TeamCard team={team} teamMatches={teamMatches} />
-          </Flex>
-        ))}
-      </Flex>
-      :
-      <Center mih="50vh">
-        <CommonAlert message="No teams selected. Please, get back to the main page and tick some" />
-      </Center>
+  const loadingComponent = (
+    <Flex align="center" justify="center" h="50vh">
+      <OverviewLoader size={50} />
+    </Flex>
   );
+
+  const noTeamsComponent = (
+    <Center mih="50vh">
+      <CommonAlert message="No teams selected. Please, get back to the main page and tick some" />
+    </Center>
+  )
+
+  const teamsComponent = (
+    <Flex mt="md" justify="center" wrap="wrap" gap="sm" direction={{ base: "column", lg: "row" }}>
+      {teamsData.map(({ team, teamMatches }, index) => (
+        <Flex key={index} flex={{ base: "1 1 100%", lg: "1 1 40%" }}>
+          <TeamCard team={team} teamMatches={teamMatches} />
+        </Flex>
+      ))}
+    </Flex>
+  );
+
+  const pageContentComponent = hasFavoriteTeams ? teamsComponent : noTeamsComponent;
+
+  return (isLoading ? loadingComponent : pageContentComponent);
 };
 
 export default FavoriteTeams;
