689cd71b00dac3aca8d06dd4fa66d22e349b5ce4
Artsem Kanapliou <k.werocky@gmail.com>
2025-02-13 17:31:09 +0100

    Add loading state to the FavoriteTeams page

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -2,3 +2,4 @@ export { default as NavigateButton } from "./NavigationButton";
 export { default as AreaSelect } from "./AreaSelect";
 export { default as CommonAlert } from "./CommonAlert";
 export { default as CountryFlag } from "./CountryFlag";
+export { default as OverviewLoader } from "./OverviewLoader";
