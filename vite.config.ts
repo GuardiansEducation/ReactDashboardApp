b8a3ffd0e281743a852c4c950d21d7089e2e4e8c
Artsem Kanapliou <k.werocky@gmail.com>
2025-03-14 17:08:15 +0100

    Remove vite paths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import sonda from 'sonda/vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -35,10 +35,18 @@ export default defineConfig(({ mode }) => {
     },
     resolve: {
       alias: {
+        "@pages": path.resolve(__dirname, "./src/pages"),
+        "@contexts": path.resolve(__dirname, "./src/contexts"),
+        "@hooks": path.resolve(__dirname, "./src/hooks"),
+        "@types": path.resolve(__dirname, "./src/types"),
+        "@store": path.resolve(__dirname, "./src/store"),
+        "@services": path.resolve(__dirname, "./src/services"),
+        "@constants": path.resolve(__dirname, "./src/constants"),
+        "@components": path.resolve(__dirname, "./src/components"),
         "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
       },
     },
-    plugins: [react(), tsconfigPaths(), sonda({
+    plugins: [react(), sonda({
       brotli: true,
       detailed: true,
       enabled: isAnalyzeMode,
